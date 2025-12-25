dfe7fa826d2fa18c9b11e72c3492bcdce2616bda
muradOS <cabiyevmuki@gmail.com>
2025-12-25 19:14:01 +0400

    Add files via upload

--- boot.asm
+++ boot.asm
@@ -1,35 +1,39 @@
 [ORG 0x7C00]
 [BITS 16]
+	
+start:
+	jmp main
 
-boot:
-	cli
+print:
+	push si
+	push ax
+	
+print_loop:
+	lodsb
+	cmp al,0
+	jz done
+	mov ah,0x0E
+	int 0x10
+	jmp print_loop
+done:
+	pop si
+	pop ax
+	ret
+
+main:
 	xor ax,ax
+	mov ds,ax
+	mov es,ax
 	mov ss,ax
 	mov sp,0x7C00
-	sti
-	mov ah,0x0E
-	mov al,'H'
-	int 0x10
+	mov si,msg
+	call print_loop
 
-	mov ah,0x0E
-	mov al,'I'
-	int 0x10
+msg:
+	hello db "Hello From Bootloader!",0
 
-	mov ah,0x0E
-	mov al,'!'
-	int 0x10
-
-kload:
-	mov dx,0x1000
-	mov ch,0
-	mov cl,2
-	mov dh,0
-	mov es,dx
-	xor bx,bx
-	mov dl,0
-	mov ah,0x02
-	int 0x13
-	call kmain
+halt:
+	jmp halt
 
 times 510-($-$$) db 0
-dw 0xAA55
+dw 0xAA55	
